f2ebdc604929e880658070b43a213668de57f44b
SyedShagufta <sweetyvox123@gmail.com>
2025-05-06 09:17:20 +0530

    Composition Challenge - My Simple version

---
+++ Practice_MAR/src/com/java/compositionChallenge/Main.java
@@ -0,0 +1,26 @@
+package com.java.compositionChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // using the getter method to call the methods
+
+        CoffeeMaker coffeeMaker = new CoffeeMaker();
+        DishWasher dishWasher = new DishWasher();
+        Refrigerator refrigerator = new Refrigerator();
+
+        SmartKitchen myKitchen = new SmartKitchen(coffeeMaker, dishWasher, refrigerator);
+
+        myKitchen.setKitchenState(true, true, true);
+        myKitchen.getBrewMaster().brewCoffee();
+        myKitchen.getIceBox().orderFood();
+        myKitchen.getDishWasher().doDishes();
+
+        // application doesn't access the objects directly
+        myKitchen.doKitchenWork();
+
+
+    }
+
+}
---
+++ Practice_MAR/src/com/java/compositionChallenge/SmartKitchen.java
@@ -0,0 +1,85 @@
+package com.java.compositionChallenge;
+
+public class SmartKitchen {
+
+    private CoffeeMaker brewMaster;
+    private DishWasher dishWasher;
+    private Refrigerator iceBox;
+
+    public SmartKitchen(CoffeeMaker brewMaster, DishWasher dishWasher, Refrigerator iceBox) {
+        this.brewMaster = brewMaster;
+        this.dishWasher = dishWasher;
+        this.iceBox = iceBox;
+    }
+
+    public void addWater(){
+        brewMaster.hasWorkToDo = true;
+    }
+
+    public void pourMilk(){
+        iceBox.hasWorkToDo = true;
+    }
+
+    public void loadDishWasher(){
+        dishWasher.hasWorkToDo = true;
+    }
+
+    public void setKitchenState(boolean coffeeMakerState, boolean refrigeratorState, boolean dishWasherState){
+        brewMaster.hasWorkToDo = coffeeMakerState;
+        iceBox.hasWorkToDo = refrigeratorState;
+        dishWasher.hasWorkToDo = dishWasherState;
+    }
+
+    public void doKitchenWork()
+    {
+        addWater();
+        brewMaster.brewCoffee();
+        pourMilk();
+        iceBox.orderFood();
+        loadDishWasher();
+        dishWasher.doDishes();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+}
+
+class Refrigerator {
+    protected boolean hasWorkToDo;
+
+    public void orderFood(){
+        if(hasWorkToDo) {
+            System.out.println("Ordering food");
+        }
+    }
+
+}
+
+class DishWasher {
+    protected boolean hasWorkToDo;
+
+    public void doDishes(){
+        if(hasWorkToDo) {
+            System.out.println("Do dishes");
+        }
+    }
+}
+
+class CoffeeMaker {
+    protected boolean hasWorkToDo;
+
+    public void brewCoffee(){
+        if(hasWorkToDo) {
+            System.out.println("Brewing coffee");
+        }
+    }
+}
